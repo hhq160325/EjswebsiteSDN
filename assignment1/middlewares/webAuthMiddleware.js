f3896ae53a59e972a6b2a69c9939b33eaee196c8
Hqmyl <142112839+hhq160325@users.noreply.github.com>
2025-03-10 00:28:25 +0700

    add css
    making css

--- assignment1/middlewares/webAuthMiddleware.js
+++ assignment1/middlewares/webAuthMiddleware.js
@@ -1,21 +1,20 @@
-// webAuthMiddleware.js
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports.webAuth = (req, res, next) => {
-    if (!req.session.token) return res.redirect('/login');  // Kiểm tra session token cho web
+  if (!req.session.token) return res.redirect("/login");
 
-    try {
-        const decoded = jwt.verify(req.session.token, process.env.JWT_SECRET);
-        req.user = decoded; 
-        next();  
-    } catch (error) {
-        res.redirect('/login'); 
-    }
+  try {
+    const decoded = jwt.verify(req.session.token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.redirect("/login");
+  }
 };
 module.exports.isAdmin = (req, res, next) => {
-    if (req.user && req.user.role === 'admin') {
-        next(); 
-    } else {
-        res.status(403).send("Bạn không có quyền truy cập.");
-    }
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(403).send("Bạn không có quyền truy cập.");
+  }
 };
